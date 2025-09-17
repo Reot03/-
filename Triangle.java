bc34a0a5d1a775cae6d8e82ea34cc6a6aed7cbbc
reot03 <104554570+Reot03@users.noreply.github.com>
2025-09-17 15:20:39 +0900

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,14 +1,19 @@
-package java0917;
+package JavaEx;
 
-public class Triangle implements Shape {
-		public double Base;
-		public double height;
+public class Triangle implements Shape {  // 삼각형 클래스 Shape 인터페이스 실행
+	public double Base;    // 삼각형 밑변 
+	public double height;  // 삼각형 높이
+	
+	public Triangle(double Base, double height) { // 생성자
+		this.Base = Base;
+		this.height = height;
+	}
 
-		@Override
-		public double getArea() {
-			return (Base*height)/2;
-		}
-		public String toString() {
-			return ("Triangle" + getArea());
-		}
+	@Override  // 면적 계산 메서드
+	public double getArea() {
+		return (Base*height)/2; //삼각형 넓이공식 (밑변 x 높이 ÷ 2)
+	}
+	public String toString() {  
+		return ("삼각형 넓이 :" + getArea());
+	}
 }
