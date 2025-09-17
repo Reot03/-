bef7d8895b9c9c871de81a8f6afa05347e380db5
reot03 <104554570+Reot03@users.noreply.github.com>
2025-09-17 15:20:17 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,18 +1,11 @@
-package java0917;
-
+package JavaEx;
 
 public class Test {
 	public static void main(String[] args) {
-		Rectangle m = new Rectangle();
-		Triangle n = new Triangle();  //도형별로 빈 객체생성
+		Rectangle R = new Rectangle(10,5);  // 직사각형하고 삼각형 객체 생성해서 생성자로 값 전달 (가로=10, 세로=5), (밑변=8, 높이=6)
+		Triangle T = new Triangle(8,6); 
 		
-		m.length = 5; //직사각형 Rectangle값
-		m.width = 4;
-		
-		n.Base = 10; //삼각형 Triangle 값
-		n.height = 5;
-		System.out.println("직사각형 : " + m.getArea()); //값 출력
-		System.out.println("삼각형 : " + n.getArea());
+		System.out.println(R); //  R.toString() 자동 호출 → 면적 출력 / * 객체를 출력하면 toString()이 자동 호출 *
+		System.out.println(T);
 	}
 }
-
